01447d91a8692676a1c53cf195c89925ece94dd8
skillet-sys <kaito.goforward0912@gmail.com>
2022-03-30 15:01:34 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# raspi-setup
+
+ラズパイのセットアップ
+
+## TeamViewer for Raspberry Pi (non-commercial use)  
+
+https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb  
---
+++ bluetoothctl.sh
@@ -0,0 +1,10 @@
+sudo bluetoothctl
+# scan on
+# scan off
+# trust E8:07:BF:xx:xx:xx
+# pair E8:07:BF:xx:xx:xx
+# connect E8:07:BF:xx:xx:xx
+# paired-devices
+# quit
+
+sudo apt install bluealsa
